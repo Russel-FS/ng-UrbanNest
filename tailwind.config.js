b94237be9c01b944aabff315ad680fae7445b2fe
RfsPaganini <frspaganini@gmail.com>
2025-02-17 19:17:24 +0000

    update setings tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  "./src/**/*.{html,ts}",
+    './src/**/*.{html,ts}',  
   ],
   theme: {
-  extend: {},
+    extend: {
+      colors: {
+        primary: '#1DA1F2',
+        secondary: '#14171A',
+        accent: '#657786',
+      },
+    },
   },
   plugins: [],
-  }
+};
