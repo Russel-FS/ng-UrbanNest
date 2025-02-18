a9cb768ce86ded8a797fb8cb6d6d16475b043141
RfsPaganini <frspaganini@gmail.com>
2025-02-18 22:06:00 +0000

    add animations in layout and home component

--- src/app/presentation/pages/home/home.component.ts
+++ src/app/presentation/pages/home/home.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(20px)' }),
+        animate('600ms cubic-bezier(0.35, 0, 0.25, 1)', 
+          style({ opacity: 1, transform: 'translateY(0)' }))
+      ])
+    ])
+  ]
 })
 export class HomeComponent {
 
