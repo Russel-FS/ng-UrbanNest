165a86154f6a327fd1b94130d73d1a20d394975e
RfsPaganini <frspaganini@gmail.com>
2025-02-18 20:37:47 +0000

    update project and add new component home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './presentation/layout/layout.component';
+import { HomeComponent } from './presentation/pages/home/home.component';
 
 export const routes: Routes = [
-    {
-        path: '', component: LayoutComponent, 
-        children: [ ]
-    },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+    ],
+   
+  },
 ];
