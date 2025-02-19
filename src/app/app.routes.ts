6813762a3a8ae57a8fdfef3ae2700b3946d0539d
RfsPaganini <frspaganini@gmail.com>
2025-02-19 23:32:20 +0000

    add new svg and include component login,register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './presentation/layout/layout.component';
 import { HomeComponent } from './presentation/pages/home/home.component';
+import { LoginComponent } from './presentation/pages/auth/login/login.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,10 @@ export const routes: Routes = [
         component: HomeComponent,
       },
     ],
-   
   },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  
 ];
