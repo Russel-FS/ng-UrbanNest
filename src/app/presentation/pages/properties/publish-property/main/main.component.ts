4e294e685a00eda2fe8156fec3fb5a8c433fe9b5
Russel-Fs <russelfloressolano900@gmail.com>
2025-02-25 14:22:52 +0000

    add service subject obersable for progress tracker

--- src/app/presentation/pages/properties/publish-property/main/main.component.ts
+++ src/app/presentation/pages/properties/publish-property/main/main.component.ts
@@ -1,24 +1,37 @@
-import { Component } from '@angular/core';
-import { ProgressTrackerComponent } from "../../../../shared/components/progress-tracker/progress-tracker.component";
-import { ProgressVerticalComponent } from "../../../../shared/components/progress-vertical/progress-vertical.component";
-import { PropertyDetailsComponent } from "../property-details/property-details.component";
- 
+import { Component, OnInit } from '@angular/core';
+import { ProgressTrackerComponent } from '../../../../shared/components/progress-tracker/progress-tracker.component';
+import { PropertyDetailsComponent } from '../property-details/property-details.component';
+import { ProgressTrackerServiceService } from '../../../../../core/services/progress-tracker-service.service';
+
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [ProgressTrackerComponent, PropertyDetailsComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
   currentStep: number = 1;
-  
+
+  constructor(private progressTrackerService: ProgressTrackerServiceService) {}
+
+  ngOnInit(): void {
+    this.progressTrackerService.progressNextStep$.subscribe(() => {
+      this.currentIncrement();
+    });
+    this.progressTrackerService.progressBackStep$.subscribe(() => {
+      this.currentDecrement();
+    });
+  }
+
   currentIncrement(): void {
     this.currentStep++;
   }
 
   currentDecrement(): void {
+    if (this.currentStep === 1) {
+      return;
+    }
     this.currentStep--;
   }
-
 }
--- src/app/presentation/pages/properties/publish-property/property-details/type-property/type-property.component.ts
+++ src/app/presentation/pages/properties/publish-property/property-details/type-property/type-property.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ProgressTrackerServiceService } from '../../../../../../core/services/progress-tracker-service.service';
+ 
 @Component({
   selector: 'app-type-property',
   imports: [],
   templateUrl: './type-property.component.html',
-  styleUrl: './type-property.component.css'
+  styleUrl: './type-property.component.css',
 })
-export class TypePropertyComponent {
+export class TypePropertyComponent implements OnInit {
+  constructor(private progressTrackerService: ProgressTrackerServiceService) {}
 
+  ngOnInit(): void {}
+
+  nextStep(): void {
+    this.progressTrackerService.nextStep();
+  }
+
+  backStep(): void {
+    this.progressTrackerService.backStep();
+  }
 }
