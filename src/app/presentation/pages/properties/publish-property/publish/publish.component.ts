39b7da23170e9c41c69f0a3c9fcc8357e42394b3
Russel FS <154459097+Corvussel@users.noreply.github.com>
2025-03-20 17:28:49 +0000

    mejorar el diseño del componente de publicación de propiedades, actualizar estilos y animaciones, y ajustar la lógica de publicación

--- src/app/presentation/pages/properties/publish-property/publish/publish.component.ts
+++ src/app/presentation/pages/properties/publish-property/publish/publish.component.ts
@@ -11,11 +11,13 @@ import { ProgressTrackerServiceService } from '../../../../../core/services/prog
   animations: [
     trigger('fadeInOut', [
       transition(':enter', [
-        style({ opacity: 0, transform: 'translateY(20px)' }),
-        animate('500ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })),
+        style({ opacity: 0, transform: 'translateY(30px)' }),
+        animate('800ms cubic-bezier(0.4, 0, 0.2, 1)', 
+          style({ opacity: 1, transform: 'translateY(0)' })),
       ]),
     ]),
   ],
+  standalone: true
 })
 export class PublishComponent {
   property = {
@@ -36,8 +38,7 @@ export class PublishComponent {
   }
 
   publish() {
-    // Lógica para publicar la propiedad
-    this.publishMessage = '¡Fantástico! Tu propiedad está ahora visible para el mundo.';
+    this.publishMessage = '¡Excepcional! Tu propiedad está lista para cautivar al mundo';
     console.log('Propiedad publicada:', this.property);
   }
 }
