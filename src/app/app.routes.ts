de98eb977f8e3ed27f9a846696f5c22008db545c
Russel-Fs <russelfloressolano900@gmail.com>
2025-03-20 21:46:31 +0000

    renombrar ruta de propiedades a 'search' y mejorar la presentación de resultados en el componente PropertyList

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -43,7 +43,7 @@ export const routes: Routes = [
             component: MainComponent,
           },
           {
-            path: 'list',
+            path: 'search',
             component: PropertyListComponent,
           },
           {
